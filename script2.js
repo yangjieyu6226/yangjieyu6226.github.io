fee6308de30d758fe06e6bba6a15f1aa81b5189e
yangjieyu6226 <100394952+yangjieyu6226@users.noreply.github.com>
2022-04-16 23:54:57 +0800

    Add files via upload

--- script2.js
+++ script2.js
@@ -8,28 +8,29 @@ function analysis(){
 }
 function finish()
   {
-  	var scores=0;
+  	var cores=0;
   	var qestions=document.getElementsByClassName("question")
   	for(var i=0;i<qestions.length;i++)
   	{
   		var count=0;
   		var flag=1;
   		var answer=document.getElementsByName("place"+(i+1));
-  		for(var j=0;j<answer.length;j++)
-  		{
-  			if(answer[j].checked)
-  			{
-  				if(answer[j].value*1<0)
-  				{
-  					flag=0;
-  					break;
-  				}
-  				else
-  					count+=answer[j].value*1;
-  			}
-  		}
-  		if(flag==1)
-  			scores+=count;
+		for(var j=0;j<answer.length;j++)
+		{
+			if(answer[j].checked)
+			{
+				if(answer[j].value*1<0)
+				{
+					flag=0;
+					break;
+				}
+				else
+					count+=answer[j].value*1;
+			}
+		}
+		if(flag==1)
+			cores+=count;
   	}
-  	alert("你的分数是 "+scores);
+  	alert("你的分数是 "+cores);
+
   }
